05a243905fe18dd716410200863ddbb78157f994
Rahul <jrahul61911@gmail.com>
2022-03-04 19:13:09 +0530

    Configurations file change commited and MAPS_DB.py renamed

--- API_FRAMEWORK/utilities/configurations.py
+++ API_FRAMEWORK/utilities/configurations.py
@@ -1,10 +1,31 @@
 import configparser
-
+import mysql.connector
+from mysql.connector import Error
 
 def getConfig():
     config = configparser.ConfigParser()
     config.read('utilities/properties.ini')
     return config
 
+
+connector_config = {
+
+    'user': getConfig()['SQL']['user'],
+    'password': getConfig()['SQL']['password'],
+    'database': getConfig()['SQL']['database'],
+    'host': getConfig()['SQL']['database']
+
+}
+
+
 def getConnection():
-    
+    try:
+      conn = mysql.connector.connect(**connector_config)
+      if conn.is_connected():
+        print("It's succesful")
+        return conn
+    except Error as e:
+        print(e)
+
+
+
